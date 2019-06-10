ca6f82b379d92f7b53e5b37c6022908e11c10b5a
admin <651386852@qq.com>
2019-06-10 18:55:47 +0800

    company

--- common/com.js
+++ common/com.js
@@ -16,3 +16,14 @@ function runAnimate(percent,dom){
 }
 
 
+function narrow(){
+    let width = $('html').width()*0.25;
+    let height = $('html').height()*0.25;
+    $('html').css({
+        position: 'absolute',
+        transform:'scale(0.5)',
+        left:-width,
+        top:-height
+    });
+}
+
